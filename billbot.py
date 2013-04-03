4e38ff561653514d56af3281af1680f93f7c215f
WILLIAM BENSON <bill@WILLIAM-BENSONs-MacBook-Pro.local>
2013-04-03 14:22:08 -0700

    Added billbot.py and billbot.xml for now.

---
+++ billbot.py
@@ -0,0 +1,35 @@
+#!/bin/python
+
+from time import sleep
+from maker_bus import *
+from myproject import *
+
+class Robot:
+
+  def __init__(self):
+
+    maker_bus_base = Maker_Bus_Base(None)
+    self.maker_bus_base = maker_bus_base
+    self.project = Project(maker_bus_base)
+
+  def run(self):
+    project = self.project
+    motor3_left = project.motor3_left
+    motor3_right = project.motor3_right
+
+    sleep(2.0)
+    motor3_left.speed_set(55)
+    motor3_right.speed_set(55)
+    sleep(2.0)
+
+    while True:
+      motor3_left.speed_set(55)
+      motor3_right.speed_set(55)
+      sleep(1.0)
+      motor3_left.speed_set(0)
+      motor3_right.speed_set(0)
+      sleep(1.0)
+
+
+robot = Robot()
+robot.run()
